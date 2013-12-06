be95c5af2772d586896305cdf6ae481042532468
cpf5193 <cpf5193@cs.washington.edu>
2013-12-06 00:29:55 -0800

    Add modal table for courses on row click

--- js/index.js
+++ js/index.js
@@ -77,261 +77,293 @@ function populateClassesTable() {
 
 
 		db("numprofsforsection", null, null, null, null, null, courseRows, function(instructors) {
-			var instuctorNum = JSON.parse(instructors);
+			var instructorNum = JSON.parse(instructors);
 			var bulkcounter = $(rows).find('.table-count');
 			for (var i = 0; i < bulkcounter.length; i++) {
-				$(bulkcounter[i]).html(instuctorNum[i]);
+				$(bulkcounter[i]).html(instructorNum[i]);
 			}
 		});
 
+		$('#class-tab .merged-row').click(courseRowClick);
 	});
 }
 
-var instr_to_all = {};
-//Instructor Name -> whole json class object
-var class_to_all = {};
-//Class Name -> whole json class object
-var instr_to_meds = {};
-//Instructor -> [med, med, med, med...]
-var class_to_meds = {};
-//Class -> [med, med, med, med...]
-var selectedRow;
-var selectedModalRow;
-
-function preprocess(jsonData) {
-	for (var i = 0; i < jsonData.length; ++i) {
-		var jsonObj = jsonData[i];
-
-		var nameTokens = jsonObj.name.split(" ");
-		var nameSansSect = "", section = nameTokens[nameTokens.length - 1];
-		nameTokens.pop();
-		for (var token = 0; token < nameTokens.length; ++token) {
-			nameSansSect += nameTokens[token];
-			if (token != nameTokens.length - 1) {
-				nameSansSect += " ";
+function courseRowClick(){
+	$('.course-header').hide();
+	$('.instr-header').show();
+	$('.modal-name').hide();
+	$('.modal-instr').show();
+	var courseName = $(this).attr('data-course');
+	var courseDept = courseName.split(" ")[0];
+	var courseNum = courseName.split(" ")[1];
+	db("getsections", null, courseDept, courseNum, null, null, null, function(instructors){
+		var instructorList = $.parseJSON(instructors),
+		container = $('#modal-tab tbody'), template = $('#modal-tab .template');
+		var savedTemplate = $(container.children()[0]);
+		container.empty();
+		container.append(savedTemplate);
+		for(var i=0; i<instructorList.length; i++){
+			temp = template.clone();
+			rowData = instructorList[i];
+			temp.find('.modal-instr').html(rowData.instructor);
+			temp.find('.modal-qtr').html(rowData.quarter);
+			temp.find('.modal-sect').html(rowData.section);
+			temp.find('.modal-median').html((parseInt(rowData.median)).toFixed(2));
+			temp.removeClass('template');
+			temp.attr('data-id', rowData.id);
+			container.append(temp);
+			if(i==0){
+				temp.trigger('click');
 			}
-		}
-		jsonObj.name = nameSansSect;
-		jsonObj.section = section;
-
-		var classSum = 0, classCount = 0, instrSum = 0, instrCount = 0;
-		var attrList = [["Amount learned:", "course"], ["The course as a whole:", "course"], ["The course content:", "course"], ["Instructor's contribution:", "instr"], ["Instructor's effectiveness:", "instr"], ["Instructor's interest:", "instr"], ["Grading Techniques:", "instr"]];
-
-		//Add up medians, delete median from list, add median measures to
-		for (var j = 0; j < attrList.length; ++j) {
-			var attr = attrList[j];
-			var attrName = attr[0], attrType = attr[1], value = jsonObj[attr[0]];
-			if (value) {
-				if (attrType == "course") {
-					classSum += value[6];
-					classCount++;
-				} else {//attrType == instr
-					instrSum += value[6];
-					instrCount++;
-				}
-				// value.pop();
-			}
-		}
-		var instrMedian = instrSum / instrCount;
-		var classMedian = classSum / classCount;
-
-		if (class_to_meds[jsonObj.name]) {
-			class_to_meds[jsonObj.name].push(classMedian);
-		} else {
-			class_to_meds[jsonObj.name] = [classMedian];
-		}
-		if (instr_to_meds[jsonObj.instructor]) {
-			instr_to_meds[jsonObj.instructor].push(instrMedian);
-		} else {
-			instr_to_meds[jsonObj.instructor] = [instrMedian];
-		}
-		if (instr_to_all[jsonObj.instructor]) {
-			instr_to_all[jsonObj.instructor].push(jsonObj);
-		} else {
-			instr_to_all[jsonObj.instructor] = [jsonObj];
-		}
-		if (class_to_all[jsonObj.name]) {
-			class_to_all[jsonObj.name].push(jsonObj);
-		} else {
-			class_to_all[jsonObj.name] = [jsonObj];
-		}
-	}
-}
-
-function renderTable(medianMap, mapToAll, table, type) {
-	var template;
-	var container = table.find('tbody');
-	var index = 0;
-	for (var medianKey in medianMap) {
-		template = table.find('.merged-row.template').clone();
-		template.find('.table-name').html(medianKey);
-		var medianArr = medianMap[medianKey];
-		var medianSum = 0, medianCount = 0;
-		for (var i = 0; i < medianArr.length; ++i) {
-			medianSum += medianArr[i];
-			medianCount++;
-		}
-		var median = medianSum / medianCount;
-		template.find('.table-median').html(median.toFixed(2));
-		template.find('.table-count').html(medianCount);
-		template.removeClass('template');
-		(function(rowData, template) {
-			template.click(function() {
-				clickHandler(rowData, template, type);
-			});
-		})(mapToAll[medianKey], template);
-
-		container.append(template);
-		index++;
-	}
-}
-
-function clickHandler(rowData, template, type) {
-	if (selectedRow) {
-		selectedRow.removeClass('selected');
-	}
-	selectedRow = template;
-	template.addClass('selected');
-	$('#rowModal').modal();
-	drawModalTable(rowData, type);
-}
-
-function tabClick() {
-	$('.ui-tabs-anchor').click(function() {
-		var tabType = $(this).html();
-		if (tabType == "Instructors") {
-			$($('.instr-table').find('tbody').children()[1]).trigger('click');
-		} else {
-			$($('.class-table').find('tbody').children()[1]).trigger('click');
 		}
 	});
 }
 
-function drawModalTable(rowData, type) {
-	var rowObj, template;
-	var mainElt, otherElt, quarter, section;
-	var container = $('.modal-table tbody');
-	var savedTemplate = $(container.children()[0]);
-	container.empty();
-	container.append(savedTemplate);
-	if (type == "Instructor") {
-		$('.instr-header').hide();
-		$('.course-header').show();
-		$('.modal-instr').hide();
-		$('.modal-name').show();
-	} else {
-		$('.course-header').hide();
-		$('.instr-header').show();
-		$('.modal-name').hide();
-		$('.modal-instr').show();
-	}
-	for (var i = 0; i < rowData.length; ++i) {
-		rowObj = rowData[i];
-		template = container.find('.template').clone();
-		if (type == "Course") {
-			template.find('.modal-instr').html(rowObj.instructor);
-		} else {
-			template.find('.modal-name').html(rowObj.name);
-		}
-		template.find('.modal-qtr').html(rowObj.quarter);
-		template.find('.modal-sect').html(rowObj.section);
+// var instr_to_all = {};
+// //Instructor Name -> whole json class object
+// var class_to_all = {};
+// //Class Name -> whole json class object
+// var instr_to_meds = {};
+// //Instructor -> [med, med, med, med...]
+// var class_to_meds = {};
+// //Class -> [med, med, med, med...]
+// var selectedRow;
+// var selectedModalRow;
 
-		var medianSum = 0, medianCount = 0, key, propVal;
-		for (key in rowObj) {
-			propVal = rowObj[key];
-			if ( typeof propVal == "object") {
-				medianSum += propVal[propVal.length - 1];
-				medianCount++;
-				// propVal.pop();
-			}
-		}
-		template.find('.modal-median').html((medianSum / medianCount).toFixed(2));
-		template.removeClass('template');
-		(function(data, type, clickedElt) {
-			template.click(function() {
-				modalClick(data, type, clickedElt);
-			});
-		})(rowObj, type, template);
-		container.append(template);
-		if (i == 0) {
-			template.trigger('click');
-		}
-	}
-}
+// function preprocess(jsonData) {
+// 	for (var i = 0; i < jsonData.length; ++i) {
+// 		var jsonObj = jsonData[i];
 
-function modalClick(data, type, selectedElt) {
-	//Mark modal row as selected
-	if (selectedModalRow) {
-		selectedModalRow.removeClass('selected');
-	}
-	selectedModalRow = selectedElt;
-	selectedElt.addClass('selected');
+// 		var nameTokens = jsonObj.name.split(" ");
+// 		var nameSansSect = "", section = nameTokens[nameTokens.length - 1];
+// 		nameTokens.pop();
+// 		for (var token = 0; token < nameTokens.length; ++token) {
+// 			nameSansSect += nameTokens[token];
+// 			if (token != nameTokens.length - 1) {
+// 				nameSansSect += " ";
+// 			}
+// 		}
+// 		jsonObj.name = nameSansSect;
+// 		jsonObj.section = section;
 
-	//Fill in chart panel heading
-	if (type == "Course") {
-		$('.main-type').html(data.name);
-		$('.other-type').html(data.instructor);
-	} else {
-		$('.main-type').html(data.instructor);
-		$('.other-type').html(data.name);
-	}
-	$('.quarter').html(data.quarter);
+// 		var classSum = 0, classCount = 0, instrSum = 0, instrCount = 0;
+// 		var attrList = [["Amount learned:", "course"], ["The course as a whole:", "course"], ["The course content:", "course"], ["Instructor's contribution:", "instr"], ["Instructor's effectiveness:", "instr"], ["Instructor's interest:", "instr"], ["Grading Techniques:", "instr"]];
 
-	//Add radio selectors
-	var container = $('.graph-selector');
-	container.empty();
-	var key, value;
-	for (key in data) {
-		value = data[key];
-		if ( typeof value == "object") {
-			var button = $(document.createElement('input'));
-			var label = $(document.createElement('label'));
-			label.html(key);
-			button.attr('type', 'radio');
-			button.attr('name', 'chart-select');
-			button.attr('value', key);
-			label.append(button);
-			container.append(label);
-		}
-	}
-	var first_radio = container.find("input")[0];
-	$(first_radio).attr('checked', 'checked');
-	$('.graph-selector input[name="chart-select"]').change(function() {
-		renderCharts($(this).val(), data[$(this).val()]);
-	});
-	$('.graph-selector input[name="chart-select"]').trigger('change');
-}
+// 		//Add up medians, delete median from list, add median measures to
+// 		for (var j = 0; j < attrList.length; ++j) {
+// 			var attr = attrList[j];
+// 			var attrName = attr[0], attrType = attr[1], value = jsonObj[attr[0]];
+// 			if (value) {
+// 				if (attrType == "course") {
+// 					classSum += value[6];
+// 					classCount++;
+// 				} else {//attrType == instr
+// 					instrSum += value[6];
+// 					instrCount++;
+// 				}
+// 				// value.pop();
+// 			}
+// 		}
+// 		var instrMedian = instrSum / instrCount;
+// 		var classMedian = classSum / classCount;
 
-function renderCharts(chartType, chartData) {
-	$('.median').html(chartData[chartData.length - 1]);
-	var noMedianData = chartData.slice(0, chartData.length - 1);
-	var data = {
-		labels : ["Excellent", "Very Good", "Good", "Fair", "Poor", "Very Poor"],
-		datasets : [{
-			fillColor : "rgba(220,220,220,0.5)",
-			strokeColor : "rgba(220,220,220,1)",
-			data : noMedianData
-		}]
-	};
-	var options = {
-		scaleSteps : 20,
-		scaleStepWidth : 5,
-		scaleStartValue : 0,
-		scaleOverride : true,
-		scaleLabel : "<%=value + '%'%>"
+// 		if (class_to_meds[jsonObj.name]) {
+// 			class_to_meds[jsonObj.name].push(classMedian);
+// 		} else {
+// 			class_to_meds[jsonObj.name] = [classMedian];
+// 		}
+// 		if (instr_to_meds[jsonObj.instructor]) {
+// 			instr_to_meds[jsonObj.instructor].push(instrMedian);
+// 		} else {
+// 			instr_to_meds[jsonObj.instructor] = [instrMedian];
+// 		}
+// 		if (instr_to_all[jsonObj.instructor]) {
+// 			instr_to_all[jsonObj.instructor].push(jsonObj);
+// 		} else {
+// 			instr_to_all[jsonObj.instructor] = [jsonObj];
+// 		}
+// 		if (class_to_all[jsonObj.name]) {
+// 			class_to_all[jsonObj.name].push(jsonObj);
+// 		} else {
+// 			class_to_all[jsonObj.name] = [jsonObj];
+// 		}
+// 	}
+// }
 
-		//Place additional graph options here
-	};
-	$('.display-chart').remove();
-	$('.chart-container').append($(document.createElement('canvas')).attr({
-		"class" : "display-chart",
-		"id" : "display-chart",
-		"height" : '350px',
-		"width" : '450px'
-	}));
-	var context = $(".display-chart").get(0).getContext("2d");
-	var barGraph = new Chart(context).Bar(data, options);
-}
+// function renderTable(medianMap, mapToAll, table, type) {
+// 	var template;
+// 	var container = table.find('tbody');
+// 	var index = 0;
+// 	for (var medianKey in medianMap) {
+// 		template = table.find('.merged-row.template').clone();
+// 		template.find('.table-name').html(medianKey);
+// 		var medianArr = medianMap[medianKey];
+// 		var medianSum = 0, medianCount = 0;
+// 		for (var i = 0; i < medianArr.length; ++i) {
+// 			medianSum += medianArr[i];
+// 			medianCount++;
+// 		}
+// 		var median = medianSum / medianCount;
+// 		template.find('.table-median').html(median.toFixed(2));
+// 		template.find('.table-count').html(medianCount);
+// 		template.removeClass('template');
+// 		(function(rowData, template) {
+// 			template.click(function() {
+// 				clickHandler(rowData, template, type);
+// 			});
+// 		})(mapToAll[medianKey], template);
+
+// 		container.append(template);
+// 		index++;
+// 	}
+// }
+
+// function clickHandler(rowData, template, type) {
+// 	if (selectedRow) {
+// 		selectedRow.removeClass('selected');
+// 	}
+// 	selectedRow = template;
+// 	template.addClass('selected');
+// 	$('#rowModal').modal();
+// 	drawModalTable(rowData, type);
+// }
+
+// function tabClick() {
+// 	$('.ui-tabs-anchor').click(function() {
+// 		var tabType = $(this).html();
+// 		if (tabType == "Instructors") {
+// 			$($('.instr-table').find('tbody').children()[1]).trigger('click');
+// 		} else {
+// 			$($('.class-table').find('tbody').children()[1]).trigger('click');
+// 		}
+// 	});
+// }
+
+// function drawModalTable(rowData, type) {
+// 	var rowObj, template;
+// 	var mainElt, otherElt, quarter, section;
+// 	var container = $('.modal-table tbody');
+// 	var savedTemplate = $(container.children()[0]);
+// 	container.empty();
+// 	container.append(savedTemplate);
+// 	if (type == "Instructor") {
+// 		$('.instr-header').hide();
+// 		$('.course-header').show();
+// 		$('.modal-instr').hide();
+// 		$('.modal-name').show();
+// 	} else {
+// 		$('.course-header').hide();
+// 		$('.instr-header').show();
+// 		$('.modal-name').hide();
+// 		$('.modal-instr').show();
+// 	}
+// 	for (var i = 0; i < rowData.length; ++i) {
+// 		rowObj = rowData[i];
+// 		template = container.find('.template').clone();
+// 		if (type == "Course") {
+// 			template.find('.modal-instr').html(rowObj.instructor);
+// 		} else {
+// 			template.find('.modal-name').html(rowObj.name);
+// 		}
+// 		template.find('.modal-qtr').html(rowObj.quarter);
+// 		template.find('.modal-sect').html(rowObj.section);
+
+// 		var medianSum = 0, medianCount = 0, key, propVal;
+// 		for (key in rowObj) {
+// 			propVal = rowObj[key];
+// 			if ( typeof propVal == "object") {
+// 				medianSum += propVal[propVal.length - 1];
+// 				medianCount++;
+// 				// propVal.pop();
+// 			}
+// 		}
+// 		template.find('.modal-median').html((medianSum / medianCount).toFixed(2));
+// 		template.removeClass('template');
+// 		(function(data, type, clickedElt) {
+// 			template.click(function() {
+// 				modalClick(data, type, clickedElt);
+// 			});
+// 		})(rowObj, type, template);
+// 		container.append(template);
+// 		if (i == 0) {
+// 			template.trigger('click');
+// 		}
+// 	}
+// }
+
+// function modalClick(data, type, selectedElt) {
+// 	//Mark modal row as selected
+// 	if (selectedModalRow) {
+// 		selectedModalRow.removeClass('selected');
+// 	}
+// 	selectedModalRow = selectedElt;
+// 	selectedElt.addClass('selected');
+
+// 	//Fill in chart panel heading
+// 	if (type == "Course") {
+// 		$('.main-type').html(data.name);
+// 		$('.other-type').html(data.instructor);
+// 	} else {
+// 		$('.main-type').html(data.instructor);
+// 		$('.other-type').html(data.name);
+// 	}
+// 	$('.quarter').html(data.quarter);
+
+// 	//Add radio selectors
+// 	var container = $('.graph-selector');
+// 	container.empty();
+// 	var key, value;
+// 	for (key in data) {
+// 		value = data[key];
+// 		if ( typeof value == "object") {
+// 			var button = $(document.createElement('input'));
+// 			var label = $(document.createElement('label'));
+// 			label.html(key);
+// 			button.attr('type', 'radio');
+// 			button.attr('name', 'chart-select');
+// 			button.attr('value', key);
+// 			label.append(button);
+// 			container.append(label);
+// 		}
+// 	}
+// 	var first_radio = container.find("input")[0];
+// 	$(first_radio).attr('checked', 'checked');
+// 	$('.graph-selector input[name="chart-select"]').change(function() {
+// 		renderCharts($(this).val(), data[$(this).val()]);
+// 	});
+// 	$('.graph-selector input[name="chart-select"]').trigger('change');
+// }
+
+// function renderCharts(chartType, chartData) {
+// 	$('.median').html(chartData[chartData.length - 1]);
+// 	var noMedianData = chartData.slice(0, chartData.length - 1);
+// 	var data = {
+// 		labels : ["Excellent", "Very Good", "Good", "Fair", "Poor", "Very Poor"],
+// 		datasets : [{
+// 			fillColor : "rgba(220,220,220,0.5)",
+// 			strokeColor : "rgba(220,220,220,1)",
+// 			data : noMedianData
+// 		}]
+// 	};
+// 	var options = {
+// 		scaleSteps : 20,
+// 		scaleStepWidth : 5,
+// 		scaleStartValue : 0,
+// 		scaleOverride : true,
+// 		scaleLabel : "<%=value + '%'%>"
+
+// 		//Place additional graph options here
+// 	};
+// 	$('.display-chart').remove();
+// 	$('.chart-container').append($(document.createElement('canvas')).attr({
+// 		"class" : "display-chart",
+// 		"id" : "display-chart",
+// 		"height" : '350px',
+// 		"width" : '450px'
+// 	}));
+// 	var context = $(".display-chart").get(0).getContext("2d");
+// 	var barGraph = new Chart(context).Bar(data, options);
+// }
 
 function db(query, prof, dept, num, cid, section, bulk_classes, callback) {
 	var getString = "?query=" + query;
